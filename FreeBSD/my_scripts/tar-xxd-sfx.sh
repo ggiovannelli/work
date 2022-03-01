eb8d06ae98a94e8d2ab41dfe70879e660f085488
Gianmarco Giovannelli <dev@gimbo.org>
2022-03-01 11:15:33 +0100

    Add files via upload

---
+++ FreeBSD/my_scripts/tar-xxd-sfx.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+if [ $# -lt 1 ]
+then
+      echo
+      echo "Syntax: $0 files1 files2 ..."
+      echo
+      exit
+fi
+
+tar -cpPf - $@ | gzip -9 | xxd -p -c 40 | awk 'BEGIN {printf("( xxd -p -r | tar -zwxPpvf - ) <<EOF\n")} {print} END {printf("EOF\n")}'
